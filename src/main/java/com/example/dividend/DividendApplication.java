b2ea940bbc8f699202f9f148b51aec73f787973d
takealook <macaroonv@gmail.com>
2024-05-03 02:41:44 +0900

    스크래핑 연습

--- src/main/java/com/example/dividend/DividendApplication.java
+++ src/main/java/com/example/dividend/DividendApplication.java
@@ -1,13 +1,48 @@
 package com.example.dividend;
 
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
 
 @SpringBootApplication
 public class DividendApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DividendApplication.class, args);
-    }
+//        SpringApplication.run(DividendApplication.class, args);
 
+        try {
+            Connection connection = Jsoup.connect("https://finance.yahoo.com/quote/COKE/history?frequency=1mo&period1=99153000&period2=1714666581");
+            Document document = connection.get();
+
+//            Elements eles = document.getElementsByAttributeValue("data-test", "historical-prices");
+            Elements eles = document.getElementsByClass("table svelte-ewueuo");
+            Element ele = eles.get(0);  // table 전체
+
+            Element tbody = ele.children().get(1);
+            for (Element e : tbody.children()) {
+                String txt = e.text();
+                if (!txt.endsWith("Dividend")) {
+                    continue;
+                }
+
+                String[] splits = txt.split(" ");
+                String month = splits[0];
+                int day = Integer.valueOf(splits[1].replace(",", ""));
+                int year = Integer.valueOf(splits[2]);
+                String dividend = splits[3];
+
+                System.out.println(year + "/" + month + "/" + day + " -> " + dividend);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
