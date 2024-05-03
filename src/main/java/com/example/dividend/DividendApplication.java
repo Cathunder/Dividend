2a996634f819d20059fc185b448289270e563307
takealook <macaroonv@gmail.com>
2024-05-04 03:46:22 +0900

    배당금 저장 기능 추가. controller, service, repository 수정

--- src/main/java/com/example/dividend/DividendApplication.java
+++ src/main/java/com/example/dividend/DividendApplication.java
@@ -1,28 +1,12 @@
 package com.example.dividend;
 
-import com.example.dividend.model.Company;
-import com.example.dividend.model.ScrapedResult;
-import com.example.dividend.scraper.Scraper;
-import com.example.dividend.scraper.YahooFinanceScraper;
-import org.jsoup.Connection;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.io.IOException;
 
 @SpringBootApplication
 public class DividendApplication {
 
     public static void main(String[] args) {
-//        SpringApplication.run(DividendApplication.class, args);
-
-        Scraper scraper = new YahooFinanceScraper();
-//        var result = scraper.scrap(Company.builder().ticker("O").build());
-        var result = scraper.scrapCompanyByTicker("MMM");
-        System.out.println(result);
+        SpringApplication.run(DividendApplication.class, args);
     }
 }
--- src/main/java/com/example/dividend/model/Company.java
+++ src/main/java/com/example/dividend/model/Company.java
@@ -1,9 +1,10 @@
 package com.example.dividend.model;
 
-import lombok.Builder;
-import lombok.Data;
+import lombok.*;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class Company {
 
